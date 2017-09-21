edb6665fcc2246d35a54b746641e8a511e29cfa7
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2017-09-21 23:19:01 +0100

    Initial Commit

---
+++ Complex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Complex
+//
+//  Created by Nicholas Outram on 21/09/2017.
+//  Copyright © 2017 Nicholas Outram. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
